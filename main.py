af3104927a43df1c80c111d7c62fa2422ce53fe0
malun22 <malun22@web.de>
2022-08-02 15:53:50 +0200

    Added some config

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from flask import Flask
+import os
+from flask import Flask, request
+from view_functionalities.analyze import Analyze
 
 app = Flask(__name__)
 
+# Constants
+TEMP_UPLOAD_FOLDER = "temp"
+MAX_USER_CONTENT_SIZE = 0.25 * 1024 * 1024 * 1024
 
-@app.route("/")
+
+@app.route("/analyze", methods=["POST"])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return Analyze.post(request)
+
+
+if __name__ == "__main__":
+
+    app.config["TEMP_UPLOAD_FOLDER"] = os.path.join(
+        app.root_path, TEMP_UPLOAD_FOLDER)
+    app.config["MAX_CONTENT_LENGTH"] = 40 * 1000 * 1000  # 40 MB
+
+    app.run(debug=True)
