b34db825d64044c7ad986f1345cac32dc33d1d1d
unknown <malun22@web.de>
2023-01-29 15:57:43 +0000

    Added basic CORS

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 import os
 from website.views import views
 from flask import Flask
+from flask_cors import CORS
 
 
 # Constants
@@ -10,6 +11,8 @@ MAX_USER_CONTENT_SIZE = 0.25 * 1024 * 1024 * 1024
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
+    
     app.secret_key = "very secret key"
 
     app.register_blueprint(views)
